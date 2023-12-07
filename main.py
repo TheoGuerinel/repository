09c0e81d1d0e0654404982b3b5c60d0e73a86450
Théo Guérinel <bauchetgwen@gmail.com>
2023-12-07 12:15:09 +0100

    maj fonction add

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 
-def add(a,b):
-    e=a+b
-    print(f'hello {e}')
-    return e
+def add():
+    a = int(input("entre un nombre entier : "))
+    b = int(input("entre un nombre entier : "))
+    e = a + b
+    print(e)
+    
 
-add(2,3)
+add()
