e8df15cafff2f55a2720b19d9143a437b2c43ccb
Théo Guérinel <bauchetgwen@gmail.com>
2023-12-07 12:37:47 +0100

    fze

--- main.py
+++ main.py
@@ -1,18 +1,7 @@
-
-<<<<<<< HEAD
 def add():
     a = int(input("entre un nombre entier : "))
     b = int(input("entre un nombre entier : "))
-    e = a - b
+    e = a + b
     print(e)
-    
 
 add()
-=======
-def add(a,b):
-    e=a+b
-    print(f'hello {e}')
-    return e
-
-add(2,3)
->>>>>>> parent of 09c0e81 (maj fonction add)
